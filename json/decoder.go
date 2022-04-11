81efdba9e99b9a7cd462700e31a7f802cd5cdc03
Sheridan C Rawlins <scr@yahooinc.com>
2022-04-10 18:37:24 -0700

    Add json.Encoder and Decoder - mainly so that (A) decode could be done from file, and (B) encoding could do pretty printing by setting the indentation.

---
+++ json/decoder.go
@@ -0,0 +1,72 @@
+package json
+
+import (
+	"encoding/json"
+	"github.com/vadv/gopher-lua-libs/io"
+	lua "github.com/yuin/gopher-lua"
+)
+
+const (
+	jsonDecoderType = "json.Decoder"
+)
+
+func CheckDecoder(L *lua.LState, n int) *json.Decoder {
+	ud := L.CheckUserData(n)
+	if decoder, ok := ud.Value.(*json.Decoder); ok {
+		return decoder
+	}
+	L.ArgError(n, jsonDecoderType+" expected")
+	return nil
+}
+
+func LVDecoder(L *lua.LState, decoder *json.Decoder) lua.LValue {
+	ud := L.NewUserData()
+	ud.Value = decoder
+	L.SetMetatable(ud, L.GetTypeMetatable(jsonDecoderType))
+	return ud
+}
+
+func DecoderDecode(L *lua.LState) int {
+	decoder := CheckDecoder(L, 1)
+	L.Pop(L.GetTop())
+	var value interface{}
+	if err := decoder.Decode(&value); err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	L.Push(decode(L, value))
+	return 1
+}
+
+func DecoderInputOffset(L *lua.LState) int {
+	decoder := CheckDecoder(L, 1)
+	L.Pop(L.GetTop())
+	L.Push(lua.LNumber(decoder.InputOffset()))
+	return 1
+}
+
+func DecoderMore(L *lua.LState) int {
+	decoder := CheckDecoder(L, 1)
+	L.Pop(L.GetTop())
+	L.Push(lua.LBool(decoder.More()))
+	return 1
+}
+
+func registerDecoder(L *lua.LState) {
+	mt := L.NewTypeMetatable(jsonDecoderType)
+	L.SetGlobal(jsonDecoderType, mt)
+	L.SetField(mt, "__index", L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
+		"decode":       DecoderDecode,
+		"input_offset": DecoderInputOffset,
+		"more":         DecoderMore,
+	}))
+}
+
+func NewDecoder(L *lua.LState) int {
+	writer := L.CheckAny(1)
+	wrapper := io.NewLuaIOWrapper(L, writer)
+	decoder := json.NewDecoder(wrapper)
+	L.Push(LVDecoder(L, decoder))
+	return 1
+}
--- json/test/test_api.lua
+++ json/test/test_api.lua
@@ -1,5 +1,6 @@
 local json = require("json")
 local inspect = require("inspect")
+local io = require("io")
 
 function TestJson(t)
     local jsonStringWithNull = [[{"a":{"b":1, "c":null}}]]
@@ -19,4 +20,38 @@ function TestJson(t)
         if result ~= jsonString then error("must be encode "..inspect(result)) end
         print("done: json.encode()")
     end)
+end
+
+function TestEncoder(t)
+    temp_file = '/tmp/tst.json'
+    os.remove(temp_file)
+    writer, err = io.open(temp_file, 'w')
+    assert(not err, err)
+    encoder = json.new_encoder(writer)
+    err = encoder:encode({foo="bar", bar="baz"})
+    assert(not err, err)
+    writer:close()
+
+    reader = io.open(temp_file, 'r')
+    contents = reader:read('*a')
+    assert(contents, "contents should not be empty")
+    contents = json.decode(contents)
+    assert(contents['foo'] == 'bar', string.format("%s ~= bar", contents['foo']))
+    assert(contents['bar'] == 'baz', string.format("%s ~= baz", contents['bar']))
+end
+
+function TestDecoder(t)
+    temp_file = '/tmp/tst.json'
+    os.remove(temp_file)
+    writer, err = io.open(temp_file, 'w')
+    assert(not err, err)
+    writer:write([[{"abc": "def", "num": 123}]])
+    writer:close()
+
+    reader = io.open(temp_file, 'r')
+    decoder = json.new_decoder(reader)
+    result, err = decoder:decode()
+    assert(not err, err)
+    assert(result['abc'] == 'def', string.format("%s ~= def", result['abc']))
+    assert(result['num'] == 123, string.format("%d ~= 123", result['num']))
 end
