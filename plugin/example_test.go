46d150e8ac9b704fc6aa840a8e104cd07d13d659
Sheridan C Rawlins <scr@yahooinc.com>
2022-08-02 09:05:19 -0700

    more spaces

--- plugin/example_test.go
+++ plugin/example_test.go
@@ -22,27 +22,27 @@ func Example_package() {
         local i = 1
         while doCh:receive() do
             print(i)
-			doneCh:send(i)
+            doneCh:send(i)
             i = i + 1
         end
     ]]
 
-	-- Make synchronization channels and fire up the plugin
-	local doCh = channel.make(100)
-	local doneCh = channel.make(100)
+    -- Make synchronization channels and fire up the plugin
+    local doCh = channel.make(100)
+    local doneCh = channel.make(100)
     local print_plugin = plugin.do_string(plugin_body, doCh, doneCh)
     print_plugin:run()
 
-	-- Allow two iterations to proceed
-	doCh:send(nil)
-	local ok, got = doneCh:receive()
-	assert(ok and got == 1, string.format("ok = %s; got = %s", ok, got))
-	doCh:send(nil)
-	ok, got = doneCh:receive()
-	assert(ok and got == 2, string.format("ok = %s; got = %s", ok, got))
+    -- Allow two iterations to proceed
+    doCh:send(nil)
+    local ok, got = doneCh:receive()
+    assert(ok and got == 1, string.format("ok = %s; got = %s", ok, got))
+    doCh:send(nil)
+    ok, got = doneCh:receive()
+    assert(ok and got == 2, string.format("ok = %s; got = %s", ok, got))
 
-	-- Close the doCh and wait to ensure it's closed gracefully but stop just to be sure
-	doCh:close()
+    -- Close the doCh and wait to ensure it's closed gracefully but stop just to be sure
+    doCh:close()
     time.sleep(1)
     print_plugin:stop()
     time.sleep(1)
