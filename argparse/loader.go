d97e8c4b9e15d00a3c418e048e9b258a89e5e3ad
Sheridan C Rawlins <scr@yahooinc.com>
2022-11-05 20:33:51 -0700

    Improve maintenance of constants (eventually they should use embed) by using textFileToGoConst

--- argparse/loader.go
+++ argparse/loader.go
@@ -1,28 +1,22 @@
 package argparse
 
-//go:generate go run internal/include_all_lua.go
-
 import (
-	"encoding/base64"
-
 	lua "github.com/yuin/gopher-lua"
 )
+
+//go:generate go run github.com/logrusorgru/textFileToGoConst@latest -in argparse.lua -o lua_const.go -c lua_argparse
 
 // Preload adds inspect to the given Lua state's package.preload table. After it
 // has been preloaded, it can be loaded using require:
 //
-//  local inspect = require("inspect")
+//	local inspect = require("inspect")
 func Preload(L *lua.LState) {
 	L.PreloadModule("argparse", Loader)
 }
 
 // Loader is the module loader function.
 func Loader(L *lua.LState) int {
-	code, err := base64.StdEncoding.DecodeString(lua_argparse)
-	if err != nil {
-		panic(err.Error())
-	}
-	if err := L.DoString(string(code)); err != nil {
+	if err := L.DoString(lua_argparse); err != nil {
 		L.RaiseError("load library 'argparse' error: %s", err.Error())
 	}
 	return 1
--- inspect/loader.go
+++ inspect/loader.go
@@ -1,28 +1,22 @@
 package inspect
 
-//go:generate go run internal/include_all_lua.go
+//go:generate go run github.com/logrusorgru/textFileToGoConst@latest -in inspect.lua -o lua_const.go -c lua_inspect
 
 import (
-	"encoding/base64"
-
 	lua "github.com/yuin/gopher-lua"
 )
 
 // Preload adds inspect to the given Lua state's package.preload table. After it
 // has been preloaded, it can be loaded using require:
 //
-//  local inspect = require("inspect")
+//	local inspect = require("inspect")
 func Preload(L *lua.LState) {
 	L.PreloadModule("inspect", Loader)
 }
 
 // Loader is the module loader function.
 func Loader(L *lua.LState) int {
-	code, err := base64.StdEncoding.DecodeString(lua_inspect)
-	if err != nil {
-		panic(err.Error())
-	}
-	if err := L.DoString(string(code)); err != nil {
+	if err := L.DoString(lua_inspect); err != nil {
 		L.RaiseError("load library 'inspect' error: %s", err.Error())
 	}
 	return 1
