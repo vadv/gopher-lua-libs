24bd69b55cfdb4be4b6b6902357edb452d7e2351
Sheridan C Rawlins <scr@yahooinc.com>
2025-06-23 16:31:37 -0700

    Add example tests.

--- hex/example_test.go
+++ hex/example_test.go
@@ -3,6 +3,7 @@ package hex
 import (
 	"log"
 
+	"github.com/vadv/gopher-lua-libs/strings"
 	lua "github.com/yuin/gopher-lua"
 )
 
@@ -11,28 +12,15 @@ func ExampleEncodeToString() {
 	defer state.Close()
 	Preload(state)
 	source := `
-    local base64 = require("base64")
-	s = base64.RawStdEncoding:encode_to_string("foo\01bar")
+    local hex = require 'hex'
+	s = hex.encode_to_string("foo\01bar")
 	print(s)
-
-	s = base64.StdEncoding:encode_to_string("foo\01bar")
-	print(s)
-
-	s = base64.RawURLEncoding:encode_to_string("this is a <tag> and should be encoded")
-	print(s)
-
-	s = base64.URLEncoding:encode_to_string("this is a <tag> and should be encoded")
-	print(s)
-
 `
 	if err := state.DoString(source); err != nil {
 		log.Fatal(err.Error())
 	}
 	// Output:
-	// Zm9vAWJhcg
-	// Zm9vAWJhcg==
-	// dGhpcyBpcyBhIDx0YWc-IGFuZCBzaG91bGQgYmUgZW5jb2RlZA
-	// dGhpcyBpcyBhIDx0YWc-IGFuZCBzaG91bGQgYmUgZW5jb2RlZA==
+	// 666f6f01626172
 }
 
 func ExampleDecodeString() {
@@ -40,30 +28,54 @@ func ExampleDecodeString() {
 	defer state.Close()
 	Preload(state)
 	source := `
-    local base64 = require("base64")
-	s, err = base64.RawStdEncoding:decode_string("Zm9vAWJhcg")
+    local hex = require 'hex'
+	s, err = hex.decode_string("666f6f01626172")
 	assert(not err, err)
 	print(s)
-
-	s, err = base64.StdEncoding:decode_string("Zm9vAWJhcg==")
-	assert(not err, err)
-	print(s)
-
-	s, err = base64.RawURLEncoding:decode_string("dGhpcyBpcyBhIDx0YWc-IGFuZCBzaG91bGQgYmUgZW5jb2RlZA")
-	assert(not err, err)
-	print(s)
-
-	s, err = base64.URLEncoding:decode_string("dGhpcyBpcyBhIDx0YWc-IGFuZCBzaG91bGQgYmUgZW5jb2RlZA==")
-	assert(not err, err)
-	print(s)
-
 `
 	if err := state.DoString(source); err != nil {
 		log.Fatal(err.Error())
 	}
 	// Output:
 	// foobar
+}
+
+func ExampleNewEncoder() {
+	state := lua.NewState()
+	defer state.Close()
+	Preload(state)
+	strings.Preload(state)
+	source := `
+    local hex = require 'hex'
+	local strings = require 'strings'
+    local writer = strings.new_builder()
+	encoder = hex.new_encoder(writer)
+	encoder:write("foo\01bar")
+	print(writer:string())
+`
+	if err := state.DoString(source); err != nil {
+		log.Fatal(err.Error())
+	}
+	// Output:
+	// 666f6f01626172
+}
+
+func ExampleNewDecoder() {
+	state := lua.NewState()
+	defer state.Close()
+	Preload(state)
+	strings.Preload(state)
+	source := `
+    local hex = require 'hex'
+	local strings = require 'strings'
+    local reader = strings.new_reader('666f6f01626172')
+	decoder = hex.new_decoder(reader)
+    local s = decoder:read("*a")
+	print(s)
+`
+	if err := state.DoString(source); err != nil {
+		log.Fatal(err.Error())
+	}
+	// Output:
 	// foobar
-	// this is a <tag> and should be encoded
-	// this is a <tag> and should be encoded
 }
