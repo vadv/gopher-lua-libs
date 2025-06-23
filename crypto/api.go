ee35ee54d29d4b980f88bed1f640ccaab0c01564
Dušan Borovčanin <borovcanindusan1@gmail.com>
2025-06-23 18:24:12 +0200

    Add AES support to crypto lib (#69)
    * Add AES encryption and decryption to crypto package
    
    Signed-off-by: Dusan Borovcanin <borovcanindusan1@gmail.com>

--- crypto/api.go
+++ crypto/api.go
@@ -4,6 +4,7 @@ package crypto
 import (
 	"crypto/md5"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 
 	lua "github.com/yuin/gopher-lua"
@@ -22,5 +23,44 @@ func SHA256(L *lua.LState) int {
 	str := L.CheckString(1)
 	hash := sha256.Sum256([]byte(str))
 	L.Push(lua.LString(fmt.Sprintf("%x", hash)))
+	return 1
+}
+
+// AESEncrypt implements AES encryption in Lua.
+func AESEncrypt(l *lua.LState) int {
+	m, key, iv, data, err := decodeParams(l)
+	if err != nil {
+		l.Push(lua.LNil)
+		l.Push(lua.LString(fmt.Sprintf("failed to decode params: %v", err)))
+		return 2
+	}
+
+	enc, err := encryptAES(m, key, iv, data)
+	if err != nil {
+		l.Push(lua.LNil)
+		l.Push(lua.LString(fmt.Sprintf("failed to encrypt: %v", err)))
+		return 2
+	}
+	l.Push(lua.LString(hex.EncodeToString(enc)))
+	return 1
+}
+
+// AESDecrypt implement AES decryption in Lua.
+func AESDecrypt(l *lua.LState) int {
+	m, key, iv, data, err := decodeParams(l)
+	if err != nil {
+		l.Push(lua.LNil)
+		l.Push(lua.LString(fmt.Sprintf("failed to decode params: %v", err)))
+		return 2
+	}
+
+	dec, err := decryptAES(mode(m), key, iv, data)
+	if err != nil {
+		l.Push(lua.LNil)
+		l.Push(lua.LString(fmt.Sprintf("failed to decrypt: %v", err)))
+		return 2
+	}
+
+	l.Push(lua.LString(hex.EncodeToString(dec)))
 	return 1
 }
